7ed91c6be868949ebc1b6e664dc10c919349a620
VanFanel <van@chaospott.de>
2025-04-17 21:01:14 +0200

    🚚 add /api to all api routes

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -16,69 +16,74 @@ import io.ktor.server.application.Application
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import kotlinx.coroutines.flow.firstOrNull
 
 
 private val logger = KotlinLogging.logger {}
 fun Application.configureRouting() {
+    // add base path// add base path
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        route("/api") {
 
-        get("/clear-devices") {
-            PSMovePairingManager.disconnectAndForgetAllPairedPSMove()
-            call.respond(HttpStatusCode.NoContent)
-        }
-
-        get("/setColor/{color}") {
-            val color = call.parameters["color"]
-            PSMoveApi.setColorOnAllMoveController(MoveColor.getColorByName(color?.uppercase()))
-            call.respond(HttpStatusCode.NoContent)
-        }
-
-        get("/setColorAnimation") {
-            PSMoveBluetoothConnectionWatcher.bluetoothConnectedPSMoves.firstOrNull()?.map { move ->
-                move.setColorAnimation(RainbowAnimation)
+            get("/") {
+                call.respondText("Hello World!")
             }
-            call.respond(HttpStatusCode.NoContent)
-        }
 
-        get("/setRedAnimationTest") {
-            PSMoveBluetoothConnectionWatcher.bluetoothConnectedPSMoves.firstOrNull()?.map { move ->
-                move.setColorAnimation(
-                    ColorAnimation(
-                        colorToSet = listOf(
-                            MoveColor.RED,
-                            MoveColor.RED_INACTIVE
-                        ), durationInMS = 1000,
-                        loop = false
+            get("/clear-devices") {
+                PSMovePairingManager.disconnectAndForgetAllPairedPSMove()
+                call.respond(HttpStatusCode.NoContent)
+            }
+
+            get("/setColor/{color}") {
+                val color = call.parameters["color"]
+                PSMoveApi.setColorOnAllMoveController(MoveColor.getColorByName(color?.uppercase()))
+                call.respond(HttpStatusCode.NoContent)
+            }
+
+            get("/setColorAnimation") {
+                PSMoveBluetoothConnectionWatcher.bluetoothConnectedPSMoves.firstOrNull()?.map { move ->
+                    move.setColorAnimation(RainbowAnimation)
+                }
+                call.respond(HttpStatusCode.NoContent)
+            }
+
+            get("/setRedAnimationTest") {
+                PSMoveBluetoothConnectionWatcher.bluetoothConnectedPSMoves.firstOrNull()?.map { move ->
+                    move.setColorAnimation(
+                        ColorAnimation(
+                            colorToSet = listOf(
+                                MoveColor.RED,
+                                MoveColor.RED_INACTIVE
+                            ), durationInMS = 1000,
+                            loop = false
+                        )
                     )
-                )
+                }
+                call.respond(HttpStatusCode.NoContent)
             }
-            call.respond(HttpStatusCode.NoContent)
-        }
 
-        get("/playsoundtest") {
-            logger.info { "play explanation..." }
-            SoundManager.asyncAddSoundToQueue(SoundId.GAME_MODE_FFA_EXPLANATION)
-            logger.info { "explanation played" }
-            call.respond(HttpStatusCode.Accepted)
-        }
-
-        get("/rumble/{intensity}") {
-            logger.info { "Rumble all move controller" }
-            val intensity = call.parameters["intensity"]?.toIntOrNull() ?: 0
-            PSMoveBluetoothConnectionWatcher.bluetoothConnectedPSMoves.firstOrNull()?.map { move ->
-                PSMoveApi.rumble(macAddress = move.macAddress, intensity = intensity )
+            get("/playsoundtest") {
+                logger.info { "play explanation..." }
+                SoundManager.asyncAddSoundToQueue(SoundId.GAME_MODE_FFA_EXPLANATION)
+                logger.info { "explanation played" }
+                call.respond(HttpStatusCode.Accepted)
             }
-            call.respond(HttpStatusCode.NoContent)
-        }
 
-        get("/accelerations") {
-            val json = AccelerationDebugger.getHistoryAsJson()
-            call.respondText(json, contentType = ContentType.Application.Json)
+            get("/rumble/{intensity}") {
+                logger.info { "Rumble all move controller" }
+                val intensity = call.parameters["intensity"]?.toIntOrNull() ?: 0
+                PSMoveBluetoothConnectionWatcher.bluetoothConnectedPSMoves.firstOrNull()?.map { move ->
+                    PSMoveApi.rumble(macAddress = move.macAddress, intensity = intensity)
+                }
+                call.respond(HttpStatusCode.NoContent)
+            }
+
+            get("/accelerations") {
+                val json = AccelerationDebugger.getHistoryAsJson()
+                call.respondText(json, contentType = ContentType.Application.Json)
+            }
         }
     }
 }
