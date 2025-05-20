53821b8170c77f0bf06816e7a21cf2ab71c945f4
VanFanel <van@chaospott.de>
2025-05-20 19:54:59 +0200

    ⚗️ try to polyfill for chrome-v46 to run the web app on old wiko

--- joustmania-web-app/vite.config.ts
+++ joustmania-web-app/vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), legacy({
+    targets: ['defaults', 'Chrome >= 46'], // oder 'Android >= 6'
+    additionalLegacyPolyfills: ['regenerator-runtime/runtime'], // for async/await
+  }),],
+  build: {
+    target: 'es5',
+    polyfillModulePreload: true,
+  }
 })
