dbf0619438c70ce305c080a2d0b541168589b5a5
VanFanel <van@chaospott.de>
2025-07-28 22:24:56 +0200

    🧑‍💻 Add script with text-to-speech integration using ElevenLabs

---
+++ development/text-to-speech-curl/index.js
@@ -0,0 +1,22 @@
+import {ElevenLabsClient, play} from '@elevenlabs/elevenlabs-js';
+import 'dotenv/config';
+
+import fs from 'fs';
+import {pipeline} from 'stream';
+import {promisify} from 'util';
+const streamPipeline = promisify(pipeline);
+
+
+console.log(process.env.VOICE_ID);
+
+const elevenlabs = new ElevenLabsClient();
+const audio = await elevenlabs.textToSpeech.convert('JBFqnCBsd6RMkjVDRZzb', {
+    text: 'Hi',
+    modelId: 'eleven_multilingual_v2',
+    outputFormat: 'mp3_44100_128',
+});
+
+
+await streamPipeline(audio, fs.createWriteStream('./output.mp3'));
+
+
